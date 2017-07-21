133fdd58b234feea48335d230c782764b3be773b
Nicholas  Vorraso <nvorraso2020@gmail.com>
2017-07-21 15:39:36 -0400

    controller & service draft

--- actions.js
+++ actions.js
@@ -1,15 +1,19 @@
 const sql = require('mssql');
 const dbConfig = require('./dbconfig');
 
-const conn = new sql.ConnectionPool( dbConfig );
+const pool1 = new sql.ConnectionPool( dbConfig );
 
 module.exports = {
+
+    sentiment_data: '',
+
+    // --- OPEN CONNECTION POOL ON SERVER START ---
 
     estConn: ( fn ) => {
 
         console.log( "Establishing service connection..");
 
-        conn.connect( err => {
+        pool1.connect( err => {
             if( err ) {
                 return console.log( err );
             } else {
@@ -19,39 +23,58 @@ module.exports = {
         })
     },
 
-    getEmp: () => {
+    // --- RETURN LATEST TIMESTAMP BY STOCK NAME ---
 
-        console.log( "Establishing service connection..");
-        
-        let req = new sql.Request(conn);
+    getLatest: ( stock_name, callback ) => {
 
-        conn.connect( err => { 
-            if ( err ) {
-                return console.log( err );
-            }
-            req.query("SELECT * FROM dbo.books", ( err, data ) => {
-                if ( err ) {
-                    console.log( err );
-                } else {
-                    console.log( data );
-                }
-                conn.close();
-            })
+        const req = new sql.Request( pool1 )
 
-        })
-    },
+        req.input('stock_name', sql.NVarChar, stock_name )
 
-    getTable: () => {
+        req.execute('dbo.getLatest', ( err, result ) => {
 
-        let req = new sql.Request(conn);
-        
-        req.query("SELECT * FROM dbo.books", ( err, data ) => {
-            if ( err ) {
-                console.log( err );
+            if( err ) {
+                console.log( err ) 
             } else {
-                console.log( data );
+                callback( result );
             }
         })
     },
 
+    // --- ADD SENTIMENT TO DB ---
+
+    addNewSentiment: ( sentiment_score, stock_url, stock_name, callback ) => {
+        const req = new sql.Request( pool1 )
+
+        req.input( 'sentiment_score', sql.Int, sentiment_score )        // <=== Sets proceedure parameters
+        req.input( 'stock_url', sql.NVarChar, stock_url )   
+        req.input( 'stock_name', sql.NVarChar, stock_name )
+
+        req.execute('dbo.addSentiment', ( err, result ) => {            // <=== Executes stored proceedure
+
+            if( err ) {
+                console.log( err );
+                callback( null ); 
+            
+            } else {
+                callback( result );
+            }
+        })
+    },
+
+    // --- DISPLAY ALL RECORDS ---
+
+    getAll: ( callback ) => {
+        const req = new sql.Request( pool1 )
+
+        req.execute( 'dbo.getAll', ( err, result ) => {
+            if( err ) {
+                console.log( err );
+                callback( null ); 
+            
+            } else {
+                callback( result );
+            }
+        })
+    }
 }
--- server.js
+++ server.js
@@ -1,20 +1,83 @@
+const bodyParser = require('body-parser');
+const dbConfig = require('./dbconfig');
 const action = require('./actions');
 const express = require('express');
-const dbConfig = require('./dbconfig');
+const path = require('path');
 const sql = require('mssql');
 const app = express();
 const port = 3000;
 
-const server = () => app.listen( port, () => console.log( `..listening on port ${ port }`));
-//app.use( () => action.getEmp() );
-action.estConn( server );
 
-app.get( '/', ( req, res ) => {
-    res.write("Hello world!");
-    res.end();
+function errorHandler (err, req, res, next) {
+  res.status(500)
+  res.render('error', { error: err })
+}
+
+app.use( errorHandler );
+app.use( express.static( __dirname + "/public") );
+app.use( bodyParser.json() );   // to support JSON-encoded bodies.
+app.use( bodyParser.urlencoded({ // to support URL-encoded bodies.
+  extended: true
+})); 
+
+const server = () => app.listen( port, () => console.log( `..listening on port ${ port }`));
+
+action.estConn( server );     // <=== Open connection pool on server start.
+
+
+// --- GET LATEST ARTICLE SENTIMENT BY STOCK NAME ---
+
+app.get( '/search', ( req, res ) => {
+
+    let stockName = req.query.stock_name;
+
+    let callback = ( data ) => {
+      if( null ) {
+
+        console.log( "Error finding record")
+      } else {
+
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify( data ));
+      }
+    }
+    action.getLatest( stockName, callback );
+
 })
 
-app.get( '/userinfo', ( req, res ) => {
-    action.getTable();
-    res.end();
+// --- ADD NEW STOCK SENTIMENT INFO ---
+
+app.get( '/store', ( req, res ) => {
+  
+    let sentimentScore = req.query.sentiment_score;
+    let stockUrl = req.query.stock_url;
+    let stockName = req.query.stock_name;
+
+    let callback = ( data ) => {
+      if ( null ) {
+
+        res.send( "Error adding record");
+      } else {  
+
+        res.send( "Added record.");
+      }
+    }
+    action.addNewSentiment( sentimentScore, stockUrl, stockName, callback );
+})
+
+// --- VIEW DB TABLE ---
+
+app.get( '/all', ( req ,res ) => {
+
+  let callback = ( data ) => {
+      if ( null ) {
+
+        res.send( "Error displaying records" );
+      } else {  
+
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify( data ));
+      }
+    }
+    action.getAll( callback );
 })
